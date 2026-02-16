ce0072b9be0b3bfb3e154f43edfb435235b98ffb
seema-1802 <seemathakor257@gmail.com>
2026-02-16 20:06:26 +0530

    Update backend URL to production

--- frontend/src/MyContext.jsx
+++ frontend/src/MyContext.jsx
@@ -2,7 +2,7 @@
 
 import { createContext } from 'react';
 
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = " https://chat-gpt-cubs.onrender.com";
 
 const MyContext = createContext();
 
